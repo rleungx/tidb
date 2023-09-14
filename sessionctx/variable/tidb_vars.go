053fe7a138f39a80c01a9b01fd7232a9d665f12b
zzm <zhouzemin@pingcap.com>
2023-09-14 23:32:51 +0800

    [cherry-pick-7.1] increase stats load sync wait time to 500ms (#631)
    * increase stats load sync wait time to 500ms
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    ---------
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>

--- sessionctx/variable/tidb_vars.go
+++ sessionctx/variable/tidb_vars.go
@@ -1221,7 +1221,7 @@ const (
 	DefTiDBTableCacheLease                         = 3 // 3s
 	DefTiDBPersistAnalyzeOptions                   = true
 	DefTiDBEnableColumnTracking                    = false
-	DefTiDBStatsLoadSyncWait                       = 100
+	DefTiDBStatsLoadSyncWait                       = 500
 	DefTiDBStatsLoadPseudoTimeout                  = true
 	DefSysdateIsNow                                = false
 	DefTiDBEnableMutationChecker                   = false
